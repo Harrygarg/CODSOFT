81ecc332df08264e84bc40608cd1c42abbab1aa5
Harrygarg <harry>
2024-05-05 11:05:41 +0530

    task 3 updated

--- task3.java
+++ task3.java
@@ -3,21 +3,22 @@
 import java.util.* ;
 
 public class task3{
-    public static int withdraw(int balance,int amount)
-    {
-        if(balance < amount)
-        {
-            balance = -1 ;
-        }
-        else
-        {
-            balance = balance - amount ;
-        }
-        return balance ;
-    }
+    // public static int withdraw(int balance,int amount)
+    // {
+    //     if(balance < amount)
+    //     {
+    //         balance = -1 ;
+    //     }
+    //     else
+    //     {
+    //         balance = balance - amount ;
+    //     }
+    //     return balance ;
+    // }
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         System.out.println("************************  ATM MACHINE  ***********************");
         System.out.println("\n                       Welcome , Dear User                   ");
 
