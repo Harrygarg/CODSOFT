1d2f8be0de34ec18f5e4db43d76fc340e14e9b33
Harrygarg <harry>
2024-05-05 11:03:58 +0530

    task 3 started

---
+++ task3.java
@@ -0,0 +1,27 @@
+// ATM MACHINE
+
+import java.util.* ;
+
+public class task3{
+    public static int withdraw(int balance,int amount)
+    {
+        if(balance < amount)
+        {
+            balance = -1 ;
+        }
+        else
+        {
+            balance = balance - amount ;
+        }
+        return balance ;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("************************  ATM MACHINE  ***********************");
+        System.out.println("\n                       Welcome , Dear User                   ");
+
+
+        sc.close();
+    }
+}
